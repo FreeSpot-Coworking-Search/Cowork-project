bee1a3627f184e10b5e1156efc416df6f6730d69
ricardozarroca <ricardozarroca@gmail.com>
2021-09-12 20:01:55 +0100

    Ajuste intro

--- front-end/src/components/Intro/Intro.js
+++ front-end/src/components/Intro/Intro.js
@@ -3,42 +3,42 @@ import './Intro.css';
 import foto1 from '../../assets/img/Foto Curriculum.jpg';
 import foto2 from '../../assets/img/DSC04573.jpg';
 export default function Intro() {
-  return (
-    <>
-      <div id="container">
-        Coworkink Projet
-        <div id="flip">
-          <div>
-            <div>comparte</div>
-          </div>
-          <div>
-            <div>encuentra</div>
-          </div>
-          <div>
-            <div>busca</div>
-          </div>
-        </div>
-        vamos!
-      </div>
-      <div className="contact">
-        <div>
-          <p>Creado por:</p>
-        </div>
-        <CreatorProfile
-          foto={foto1}
-          nombre={'Daniel Martinez'}
-          linkedIn="https://www.linkedin.com/in/danielmartinezgonzalez-developer/"
-          gitHub="https://github.com/j-coast"
-          mail="jcoastmail@gmail.com"
-        />
-        <CreatorProfile
-          foto={foto2}
-          nombre={'Ricardo Zarroca'}
-          linkedIn="https://www.linkedin.com/in/rzarroca"
-          gitHub="https://github.com/rzarroca"
-          mail="ricardozarroca@gmail.com"
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div id="container">
+                Coworking Project
+                <div id="flip">
+                    <div>
+                        <div>comparte</div>
+                    </div>
+                    <div>
+                        <div>encuentra</div>
+                    </div>
+                    <div>
+                        <div>busca</div>
+                    </div>
+                </div>
+                vamos!
+            </div>
+            <div className="contact">
+                <div>
+                    <p>Creado por:</p>
+                </div>
+                <CreatorProfile
+                    foto={foto1}
+                    nombre={'Daniel Martinez'}
+                    linkedIn="https://www.linkedin.com/in/danielmartinezgonzalez-developer/"
+                    gitHub="https://github.com/j-coast"
+                    mail="jcoastmail@gmail.com"
+                />
+                <CreatorProfile
+                    foto={foto2}
+                    nombre={'Ricardo Zarroca'}
+                    linkedIn="https://www.linkedin.com/in/rzarroca"
+                    gitHub="https://github.com/rzarroca"
+                    mail="ricardozarroca@gmail.com"
+                />
+            </div>
+        </>
+    );
 }
