9c7abac7143e4c7257f4ca10804a8ffd848a6064
ricardozarroca <ricardozarroca@gmail.com>
2021-09-24 11:09:34 +0100

    Ajuste para build de links en MyCenterSelectors

--- front-end/src/components/DateRange/DateRange.js
+++ front-end/src/components/DateRange/DateRange.js
@@ -6,54 +6,54 @@ import { DateRange } from 'react-date-range';
 import { eachDayOfInterval } from 'date-fns';
 
 export default function DateRangeSelector({
-  setNewSearchObject,
-  newSearchObject,
-  minDate,
-  reserves,
+    setNewSearchObject,
+    newSearchObject,
+    minDate,
+    reserves,
 }) {
-  const handleSelect = (event) => {
-    setSelectionRange({
-      startDate: event.selection.startDate,
-      endDate: event.selection.endDate,
-      key: 'selection',
+    const handleSelect = (event) => {
+        setSelectionRange({
+            startDate: new Date(event.selection.startDate),
+            endDate: new Date(event.selection.endDate),
+            key: 'selection',
+        });
+        setNewSearchObject({
+            ...newSearchObject,
+            fecha_entrada: new Date(event.selection.startDate),
+            fecha_salida: new Date(event.selection.endDate),
+        });
+    };
+    const [selectionRange, setSelectionRange] = useState({
+        startDate: newSearchObject.fecha_entrada
+            ? new Date(newSearchObject.fecha_entrada)
+            : new Date(),
+        endDate: newSearchObject.fecha_salida
+            ? new Date(newSearchObject.fecha_salida)
+            : new Date(),
+        key: 'selection',
     });
-    setNewSearchObject({
-      ...newSearchObject,
-      fecha_entrada: event.selection.startDate,
-      fecha_salida: event.selection.endDate,
-    });
-  };
-  const [selectionRange, setSelectionRange] = useState({
-    startDate: newSearchObject.fecha_entrada
-      ? new Date(newSearchObject.fecha_entrada)
-      : new Date(),
-    endDate: newSearchObject.fecha_salida
-      ? new Date(newSearchObject.fecha_salida)
-      : new Date(),
-    key: 'selection',
-  });
 
-  let disabledDates = useMemo(() => {
-    let dates = [];
-    reserves?.forEach((reserve) => {
-      const reserveArray = eachDayOfInterval({
-        start: new Date(reserve.fecha_inicio),
-        end: new Date(reserve.fecha_fin),
-      });
-      dates = dates.concat(reserveArray);
-    });
-    return dates;
-  }, [reserves]);
+    let disabledDates = useMemo(() => {
+        let dates = [];
+        reserves?.forEach((reserve) => {
+            const reserveArray = eachDayOfInterval({
+                start: new Date(reserve.fecha_inicio),
+                end: new Date(reserve.fecha_fin),
+            });
+            dates = dates.concat(reserveArray);
+        });
+        return dates;
+    }, [reserves]);
 
-  return (
-    <div className="dateRange">
-      <DateRange
-        ranges={[selectionRange]}
-        rangeColors={['rgba(var(--primary),0.6)']}
-        onChange={(event) => handleSelect(event)}
-        minDate={minDate ? new Date(minDate) : new Date()}
-        disabledDates={disabledDates}
-      />
-    </div>
-  );
+    return (
+        <div className="dateRange">
+            <DateRange
+                ranges={[selectionRange]}
+                rangeColors={['rgba(var(--primary),0.6)']}
+                onChange={(event) => handleSelect(event)}
+                minDate={minDate ? new Date(minDate) : new Date()}
+                disabledDates={disabledDates}
+            />
+        </div>
+    );
 }
--- front-end/src/components/MyCenterSelector/MyCenterSelector.js
+++ front-end/src/components/MyCenterSelector/MyCenterSelector.js
@@ -1,31 +1,35 @@
 import './MyCenterSelector.css';
 import '../../css/presentation.css';
+import { Link } from 'react-router-dom';
 
 export default function MyCenterSelector({
-  centers,
-  selectedCenter,
-  setSelectedCenter,
+    centers,
+    selectedCenter,
+    setSelectedCenter,
 }) {
-  return (
-    <div className="myCenterSelector">
-      <nav>
-        <a href={`/center/${centers[selectedCenter].id}`}>
-          {centers[selectedCenter].nombre}
-        </a>
-        <ul>
-          {centers.map((center, index) =>
-            index !== selectedCenter ? (
-              <li key={center.id}>
-                <a href="#" onClick={() => setSelectedCenter(index)}>
-                  {centers[index].nombre}
-                </a>
-              </li>
-            ) : (
-              ''
-            )
-          )}
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="myCenterSelector">
+            <nav>
+                <Link to={`/center/${centers[selectedCenter].id}`}>
+                    {centers[selectedCenter].nombre}
+                </Link>
+                <ul>
+                    {centers.map((center, index) =>
+                        index !== selectedCenter ? (
+                            <li key={center.id}>
+                                <a
+                                    href="#"
+                                    onClick={() => setSelectedCenter(index)}
+                                >
+                                    {centers[index].nombre}
+                                </a>
+                            </li>
+                        ) : (
+                            ''
+                        )
+                    )}
+                </ul>
+            </nav>
+        </div>
+    );
 }
