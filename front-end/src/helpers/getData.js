6985d82a925ea7ba0c0643423f673c021f96000d
j-coast <jcoastmail@gmail.com>
2021-10-07 11:32:17 +0200

    Correccion getData

--- front-end/src/helpers/getData.js
+++ front-end/src/helpers/getData.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
 export default async function getData(endPoint, queryParams) {
-    const {
-        REACT_APP_API_LOCAL_SERVER_HOST: host,
-        REACT_APP_API_LOCAL_SERVER_PORT: port,
-    } = process.env;
-    const registrationRoute = `${host}:${port}${endPoint}`;
-    try {
-        const response = await axios.get(registrationRoute, {
-            params: queryParams,
-        });
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+  const { REACT_APP_API_LOCAL_SERVER_HOST: host } = process.env;
+  const registrationRoute = `${host}${endPoint}`;
+  try {
+    const response = await axios.get(registrationRoute, {
+      params: queryParams,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
