b3aea208e1d2c1edcf508c30e39ec33c3e404e03
ricardozarroca <ricardozarroca@gmail.com>
2021-10-14 00:41:44 +0100

    getHost() agregado a componentes CardImageSlide y ScoreCard

--- front-end/src/components/CenterCard/CenterCard.js
+++ front-end/src/components/CenterCard/CenterCard.js
@@ -1,47 +1,52 @@
 import StarsDisplay from '../StarsDisplay/StarsDisplay';
 import { Link } from 'react-router-dom';
 import './CenterCard.css';
-import image from '../../assets/img/Coworking-768x380.png';
 import objectToQuerryParamsString from '../../helpers/objectToQuerryParamsString';
 import CardImageSlide from '../CardImageSlide/CardImageSlide';
 
 export default function ListCentersSearchElement({
-  center,
-  searchObject,
-  linksRoute,
+    center,
+    searchObject,
+    linksRoute,
 }) {
-  return (
-    <li className="centerCard">
-      <Link
-        to={objectToQuerryParamsString(
-          '/search/space',
-          { id_centro: center.id },
-          searchObject
-        )}
-        className="leftColumn"
-      >
-        <div>
-          <p className="centerCardName">{center.nombre}</p>
-          <h4 className="centerCardPlace">({center.localidad})</h4>
-        </div>
-        <div className="centerData">
-          <p>
-            {center.precio_minimo}-{center.precio_maximo}€
-          </p>
-          <StarsDisplay puntuation={center.puntuacion_media}></StarsDisplay>
-        </div>
-      </Link>
-      <div className="rightColumn">
-        {center.imagenes ? (
-          <CardImageSlide
-            images={center.imagenes}
-            tag={center.id}
-            className="imageSlide"
-          />
-        ) : (
-          <img src={center.imagenes[0]} className="centerCardImage" alt="" />
-        )}
-      </div>
-    </li>
-  );
+    return (
+        <li className="centerCard">
+            <Link
+                to={objectToQuerryParamsString(
+                    '/search/space',
+                    { id_centro: center.id },
+                    searchObject
+                )}
+                className="leftColumn"
+            >
+                <div>
+                    <p className="centerCardName">{center.nombre}</p>
+                    <h4 className="centerCardPlace">({center.localidad})</h4>
+                </div>
+                <div className="centerData">
+                    <p>
+                        {center.precio_minimo}-{center.precio_maximo}€
+                    </p>
+                    <StarsDisplay
+                        puntuation={center.puntuacion_media}
+                    ></StarsDisplay>
+                </div>
+            </Link>
+            <div className="rightColumn">
+                {center.imagenes ? (
+                    <CardImageSlide
+                        images={center.imagenes}
+                        tag={center.id}
+                        className="imageSlide"
+                    />
+                ) : (
+                    <img
+                        src={center.imagenes[0]}
+                        className="centerCardImage"
+                        alt=""
+                    />
+                )}
+            </div>
+        </li>
+    );
 }
